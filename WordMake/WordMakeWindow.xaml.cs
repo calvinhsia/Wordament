fa77f0884016a618a3c2eef7ab6d0f9af6f354ee
Calvin Hsia <calvinh@microsoft.com>
2019-03-08 09:53:51 -0800

    wm ui xaml

--- WordMake/WordMakeWindow.xaml.cs
+++ WordMake/WordMakeWindow.xaml.cs
@@ -23,23 +23,29 @@ namespace WordMake
     {
         int _seed;
         Random _Random;
+        DictionaryLib.DictionaryLib dict;
         public WordMakeWindow()
         {
             InitializeComponent();
-            this.Loaded += WordMakeWindow_Loaded;
+            //            this.Loaded += WordMakeWindow_Loaded;
+            this.Loaded += (o, e) =>
+              {
+                  _seed = Environment.TickCount;
+                  if (Debugger.IsAttached)
+                  {
+                      _seed = 1;
+                  }
+                  _Random = new Random(_seed);
+                  dict = new DictionaryLib.DictionaryLib(DictionaryLib.DictionaryType.Small, _Random);
+                  ChkSubWords_Unchecked(o, e);
+                  BtnNext_Click(o, e);
+              };
         }
 
         private void WordMakeWindow_Loaded(object sender, RoutedEventArgs e)
         {
             try
             {
-                _seed = Environment.TickCount;
-                if (Debugger.IsAttached)
-                {
-                    _seed = 1;
-                }
-                _Random = new Random(_seed);
-                var dict = new DictionaryLib.DictionaryLib(DictionaryLib.DictionaryType.Small, _Random);
                 var spControls = new StackPanel()
                 {
                     Orientation = Orientation.Vertical
@@ -63,22 +69,6 @@ namespace WordMake
                 spControls.Children.Add(lst);
                 void BtnNext_OnClick()
                 {
-                    var isGoodWord = false;
-                    string randword = string.Empty;
-                    while (!isGoodWord)
-                    {
-                        randword = dict.RandomWord();
-                        var randwordLets = randword.OrderBy(l => l).Distinct();
-                        if (randwordLets.Count() > 4)
-                        {
-                            isGoodWord = true;
-                        }
-                    }
-                    txtWord.Text = randword;
-                    var subwords = dict.FindAnagrams(randword, DictionaryLib.DictionaryLib.AnagramType.SubWord5);
-//                    var subwords = dict.FindSubWordsFromLetters(randword, DictionaryLib.DictionaryLib.AnagramType.SubWord5);
-                    txtSubWordCnt.Text = subwords.Count().ToString();
-                    lst.ItemsSource = subwords;
 
                 }
                 btnNext.Click += (ob, eb) => BtnNext_OnClick();
@@ -88,6 +78,41 @@ namespace WordMake
             {
                 this.Content = ex.ToString();
             }
+        }
+
+        private void BtnNext_Click(object sender, RoutedEventArgs e)
+        {
+            var isGoodWord = false;
+            string randword = string.Empty;
+            while (!isGoodWord)
+            {
+                randword = dict.RandomWord();
+                var randwordLets = randword.OrderBy(l => l).Distinct();
+                if (randwordLets.Count() > 4)
+                {
+                    isGoodWord = true;
+                }
+            }
+            this.txtRootWord.Text = randword;
+            var subwords = dict.FindAnagrams(randword, DictionaryLib.DictionaryLib.AnagramType.SubWord5);
+            //                    var subwords = dict.FindSubWordsFromLetters(randword, DictionaryLib.DictionaryLib.AnagramType.SubWord5);
+            this.txtResultCount.Text = subwords.Count().ToString();
+            this.lstResults.ItemsSource = subwords;
+
+        }
+
+        private void ChkSubWords_Checked(object sender, RoutedEventArgs e)
+        {
+            this.lblSubAnagramType.Content = "SubWord minimum length";
+            this.cboAnagramType.ItemsSource = new[] { 3, 4, 5, 6, 7 };
+            this.cboAnagramType.SelectedIndex = 2;
+        }
+
+        private void ChkSubWords_Unchecked(object sender, RoutedEventArgs e)
+        {
+            this.lblSubAnagramType.Content = "SubAnagram minimum length";
+            this.cboAnagramType.ItemsSource = new[] { 3, 4, 5, 6 };
+            this.cboAnagramType.SelectedIndex = 3;
         }
     }
 }
